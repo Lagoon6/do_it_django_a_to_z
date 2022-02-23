b9722a2faec33f9fa5839a7a24949b8f8276cc39
DESKTOP-1NGUJ0F\admin <arkanum3608@naver.com>
2022-02-23 16:02:11 +0900

    Django 프로젝트 생성

---
+++ do_it_django_prj/urls.py
@@ -0,0 +1,25 @@
+"""do_it_django_prj URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+'''
+사용자가 어떤 url형식으로 접근했을 때 어떻게 웹사이트를 작동시킬지를 정리해 놓은 파일
+'''
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
