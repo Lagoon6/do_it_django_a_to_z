57cbd3f8c412ae3fde7c979f37ef92c4f1ce3bbd
DESKTOP-1NGUJ0F\admin <arkanum3608@naver.com>
2022-02-23 17:08:44 +0900

    FBV로 블로그 포스트 상세 페이지 만들기

--- blog/models.py
+++ blog/models.py
@@ -11,3 +11,7 @@ class Post(models.Model):
 
     def __str__(self):
         return f'[{self.pk}]{self.title}'   #pk: 각 레코드에 대한 고유의 값./ title: 제목
+
+
+    def get_absolute_url(self):
+        return f'/blog/{self.pk}/'
--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     # 이 부분을 채울 겁니다!
+    path('<int:pk>/', views.single_post_page),
     path('',views.index),
 ]
